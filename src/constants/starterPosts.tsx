e2df785764be84c87f39c6e22762da024860fddf
Grant Reighard <grant.reighard@gmail.com>
2022-04-26 18:33:07 -0400

    Adds profile picture and starter posts.

---
+++ src/constants/starterPosts.tsx
@@ -0,0 +1,38 @@
+const STARTER_POSTS = [
+    {
+        user: "Nickmercs",
+        timestamp: 1651011986288,
+        content: "Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis.",
+        stats: {
+            hypes: 100,
+            comments: 25,
+            shares: 12,
+            views: 100
+        },
+        comments: []
+    },{
+        user: "Nickmercs",
+        timestamp: 1651011986288,
+        content: "Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis.",
+        stats: {
+            hypes: 100,
+            comments: 25,
+            shares: 12,
+            views: 100
+        },
+        comments: [
+            {
+                title: "Title goes here",
+                subtitle: "Secondary title goes here",
+                content: "Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis.",
+                stats: {
+                    hypes: 100,
+                    replies: 25,
+                    shares: 12,
+                }
+            }
+        ]
+    }
+]
+
+export default STARTER_POSTS;
