dad4c4b79b072ccded0d6dc01cdea9af94e2fbc0
Grant Reighard <grant.reighard@gmail.com>
2022-04-26 15:20:52 -0400

    Adds feed component and styles.

---
+++ src/components/feed.tsx
@@ -0,0 +1,13 @@
+import styles from './feed.module.css';
+
+const Feed = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.feed}>
+                Feed
+            </div>
+        </div>
+    )
+}
+
+export default Feed;
