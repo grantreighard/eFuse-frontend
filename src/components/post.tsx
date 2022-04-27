121c01bf242cb3f766f8ecef32c6954d6a0810a6
Grant Reighard <grant.reighard@gmail.com>
2022-04-26 20:29:20 -0400

    Builds out statBar and commentBar.

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -52,6 +52,32 @@ const Post: React.FC<IPost> = ({ post }: IPost) => {
             <div className={styles.content}>
                 {post.content}
             </div>
+            <div className={styles.statBar}>
+                <div className={styles.singleStat}>
+                    <FontAwesomeIcon icon={["fal", "droplet"]} className={styles.statIcon} />
+                    <span className={styles.stat}>{post.stats.hypes}</span>
+                    <span className={styles.statLabel}>Hypes</span>
+                </div>
+                <div className={styles.singleStat}>
+                    <FontAwesomeIcon icon={["fal", "message"]} className={styles.statIcon}  />
+                    <span className={styles.stat}>{post.stats.comments}</span>
+                    <span className={styles.statLabel}>Comments</span> 
+                </div>
+                <div className={styles.singleStat}>
+                    <FontAwesomeIcon icon={["fal", "share-nodes"]} className={styles.statIcon}  />
+                    <span className={styles.stat}>{post.stats.shares}</span>
+                    <span className={styles.statLabel}>Shares</span> 
+                </div>
+                <div className={styles.singleStat}>
+                    <span className={styles.stat}>{post.stats.views}</span>
+                    <span className={styles.statLabel}>Views</span> 
+                </div>
+            </div>
+            <div className={styles.commentBar}>
+                <FontAwesomeIcon icon={["fal", "message"]} className={styles.commentLeftIcon}  />
+                <input className={styles.addComment} placeholder="Add comment" />
+                <FontAwesomeIcon icon={["fal", "circle-plus"]} className={styles.commentRightIcon}  />
+            </div>
         </div>
     )
 }
